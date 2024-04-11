f9d6c091c3865ce790f3d4d4541b81dbfdf145a6
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-04-11 11:07:02 +0900

    :heavy_plus_sign: Add `@radix-ui/themes` to dependency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,68 +8,15 @@ import { RecoilRoot } from "recoil";
 
 import "../style.css";
 import "../App.css";
+import { Theme } from "@radix-ui/themes";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [openSideMenu, setOpenSideMenu] = useState(false);
-  useEffect(() => {
-    const unlisten = (async () =>
-      await listen("check-update", (event) => {
-        void (async (event) => {
-          if (event.payload as boolean) {
-            void message("This is latest version of 労働中");
-          } else {
-            const result = await ask(
-              "Open download page?",
-              "New version of 労働中 are available!"
-            );
-            if (result) {
-              void open(
-                "https://github.com/ChanTsune/roudoutyu/releases/latest"
-              );
-            }
-          }
-        })(event);
-      }))();
-    return () => {
-      void (async () => (await unlisten)())();
-    };
-  }, []);
-
-  const menuItems = [
-    {
-      name: "Timer",
-      href: "/",
-    },
-  ];
   return (
     <RecoilRoot>
-      <div
-        style={{
-          display: "flex",
-          height: "100vh",
-        }}
-      >
-        <aside
-          className={openSideMenu ? "sidebar" : "sidebar closed"}
-          style={{
-            flexGrow: openSideMenu ? 1 : 0,
-          }}
-          onClick={() => setOpenSideMenu((v) => !v)}
-        >
-          {openSideMenu &&
-            menuItems.map((item) => {
-              return (
-                <Link className="item" href={item.href} key={item.name}>
-                  {item.name}
-                </Link>
-              );
-            })}
-        </aside>
-        <main style={{ flexGrow: 4 }}>
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <Theme appearance="dark">
+        <Component {...pageProps} />
+      </Theme>
     </RecoilRoot>
   );
 }
