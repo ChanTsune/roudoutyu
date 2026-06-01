4312fd9e5d5c4883072683f1d0dcf1e557db48dd
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2026-06-01 13:22:17 +0900

    Remove timer sidebar layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,14 @@
 import type { AppProps } from "next/app";
-import Link from "next/link";
 import { listen } from "@tauri-apps/api/event";
 import { open } from "@tauri-apps/plugin-shell";
 import { ask, message } from "@tauri-apps/plugin-dialog";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { RecoilRoot } from "recoil";
 
 import "../style.css";
-import "../App.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [openSideMenu, setOpenSideMenu] = useState(false);
   useEffect(() => {
     const unlisten = (async () =>
       await listen("check-update", (event) => {
@@ -36,40 +33,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     };
   }, []);
 
-  const menuItems = [
-    {
-      name: "Timer",
-      href: "/",
-    },
-  ];
   return (
     <RecoilRoot>
-      <div
-        style={{
-          display: "flex",
-          height: "100vh",
-        }}
-      >
-        <aside
-          className={openSideMenu ? "sidebar" : "sidebar closed"}
-          style={{
-            flexGrow: openSideMenu ? 1 : 0,
-          }}
-          onClick={() => setOpenSideMenu((v) => !v)}
-        >
-          {openSideMenu &&
-            menuItems.map((item) => {
-              return (
-                <Link className="item" href={item.href} key={item.name}>
-                  {item.name}
-                </Link>
-              );
-            })}
-        </aside>
-        <main style={{ flexGrow: 4 }}>
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <Component {...pageProps} />
     </RecoilRoot>
   );
 }
